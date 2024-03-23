a59afb3bc53977fd2b00ca7ae0c0a3466fc88275
ky0he1 <kyouta1218@gmail.com>
2024-03-23 13:34:18 +0900

    Add unary plus and minus

--- main.c
+++ main.c
@@ -142,6 +142,7 @@ typedef enum {
     ND_SUB, // -
     ND_MUL, // *
     ND_DIV, // /
+    ND_NEG, // unary -
     ND_NUM, // Integer
 } NodeKind;
 
@@ -170,6 +171,13 @@ new_binary(NodeKind kind, Node* lhs, Node* rhs) {
 }
 
 static Node*
+new_unary(NodeKind kind, Node* expr) {
+    Node* node = new_node(kind);
+    node->lhs = expr;
+    return node;
+}
+
+static Node*
 new_num(int val) {
     Node* node = new_node(ND_NUM);
     node->val = val;
@@ -180,6 +188,8 @@ static Node*
 expr(Token** rest, Token* tok);
 static Node*
 mul(Token** rest, Token* tok);
+static Node*
+unary(Token** rest, Token* tok);
 static Node*
 primary(Token** rest, Token* tok);
 
@@ -204,25 +214,40 @@ expr(Token** rest, Token* tok) {
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 static Node*
 mul(Token** rest, Token* tok) {
-    Node* node = primary(&tok, tok);
+    Node* node = unary(&tok, tok);
 
     for (;;) {
         if (equal(tok, "*")) {
-            node = new_binary(ND_MUL, node, primary(&tok, tok->next));
+            node = new_binary(ND_MUL, node, unary(&tok, tok->next));
             continue;
         }
 
         if (equal(tok, "/")) {
-            node = new_binary(ND_DIV, node, primary(&tok, tok->next));
+            node = new_binary(ND_DIV, node, unary(&tok, tok->next));
             continue;
         }
 
         *rest = tok;
         return node;
     }
+}
+
+// unary = ("+" | "-") unary
+//       | primary
+static Node*
+unary(Token** rest, Token* tok) {
+    if (equal(tok, "+")) {
+        return unary(rest, tok->next);
+    }
+
+    if (equal(tok, "-")) {
+        return new_unary(ND_NEG, unary(rest, tok->next));
+    }
+
+    return primary(rest, tok);
 }
 
 // primary = "(" expr ")" | num
@@ -263,9 +288,14 @@ pop(char* arg) {
 
 static void
 gen_expr(Node* node) {
-    if (node->kind == ND_NUM) {
-        printf("  mov $%d, %%rax\n", node->val);
-        return;
+    switch (node->kind) {
+        case ND_NUM:
+            printf("  mov $%d, %%rax\n", node->val);
+            return;
+        case ND_NEG:
+            gen_expr(node->lhs);
+            printf("  neg %%rax\n");
+            return;
     }
 
     gen_expr(node->rhs);
